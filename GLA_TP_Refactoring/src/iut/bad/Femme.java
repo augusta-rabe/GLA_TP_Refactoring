68096a9efbe7f4c3632e89695abd5020c18f216c
augusta-rabe <146953117+augusta-rabe@users.noreply.github.com>
2024-03-31 14:55:50 +0300

    Modification de la classe Homme/Femme pour que la classe Humain soit leur classe parente

--- GLA_TP_Refactoring/src/iut/bad/Femme.java
+++ GLA_TP_Refactoring/src/iut/bad/Femme.java
@@ -1,50 +1,11 @@
 package iut.bad;
 
-public class Femme {
-    private String nom;
-    private String prenom;
-    private int age;
-
+public class Femme extends Humain {
     public Femme(String nom, String prenom, int age) {
-        this.nom = nom;
-        this.prenom = prenom;
-        this.age = age;
+        super(nom, prenom, age);
     }
 
-    // Getters and setters
-    public String getNom() {
-        return nom;
-    }
+    // Aucun champ à ajouter, car les champs sont déjà définis dans la classe parente Humain
 
-    public void setNom(String nom) {
-        this.nom = nom;
-    }
-
-    public String getPrenom() {
-        return prenom;
-    }
-
-    public void setPrenom(String prenom) {
-        this.prenom = prenom;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    // Méthode toString() pour l'affichage
-    @Override
-    public String toString() {
-        return "Femme [nom=" + nom + ", prenom=" + prenom + ", age=" + age + "]";
-    }
-
-    // Méthode main pour tester
-    public static void main(String[] args) {
-        Femme f = new Femme("Doe", "Jane", 25);
-        System.out.println(f.toString());
-    }
+    // Aucune méthode à redéfinir ici, car toString() et les getters/setters sont déjà dans Humain
 }
